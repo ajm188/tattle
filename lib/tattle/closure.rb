e7dda9acf11e1693853df038950d63ad949b6102
Andrew Mason <mason@case.edu>
2015-02-28 03:18:13 -0500

    adding more implementation to Closure.compute

--- lib/tattle/closure.rb
+++ lib/tattle/closure.rb
@@ -2,13 +2,30 @@ module Tattle
   class Closure
     def self.compute(ast, current_closure)
       if ast.module?
-        current_closure[ast.module_name] =
-          Closure.compute(ast.module_body,
-                          Closure.new({},
-                                      current_closure))
-      else
-        current_closure
-      end
+        current_closure.add(ast.module_name,
+                            Closure.compute(ast.module_body,
+                                            Closure.new({},
+                                                        current_closure)))
+      elsif ast.class?
+        current_closure.add(ast.class_name,
+                            Closure.compute(ast.class_body,
+                                            Closure.new({},
+                                                        current_closure)))
+      elsif ast.method_def?
+        current_closure.add(ast.method_name,
+                            Closure.compute(ast.method_body,
+                                            Closure.new({},
+                                            current_closure)))
+      elsif ast.block? # ast.proc? ast.lambda?
+        # ???
+      elsif ast.local_assignment?
+        current_closure.add(ast.assignment_left_side)
+      elsif ast.instance_var_assignment?
+        current_closure.parent.add(ast.assignment_left_side)
+      elsif ast.class_var_assignment?
+        current_closure.parent.parent.add(ast.assignment_left_hand_side)
+      end 
+      current_closure
     end
 
     def initialize(closure = {}, parent = nil)
